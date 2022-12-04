f88bd1eb6fd6988c4205476bb20ef5785c08ed0d
qunash <anzoria@gmail.com>
2022-12-04 19:54:49 +0400

    +search()

--- api/index.py
+++ api/index.py
@@ -9,3 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/search')
+def search():
+    keywords = request.args.get('keywords')
+    region = request.args.get('region', 'wt-wt')
+    safesearch = request.args.get('safesearch', 'Off')
+    time = request.args.get('time', None)
+    max_results = request.args.get('max_results', None)
+
+    results = ddg(keywords, region=region, safesearch=safesearch, time=time, max_results=max_results)
+
+    return jsonify(results)
